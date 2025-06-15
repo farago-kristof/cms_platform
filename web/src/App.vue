accfe1139fd1c0a5aed4580c5790c27f08d17afc
kristof-farago <kristof.farago@nixs.com>
2025-06-15 17:39:45 +0200

    POC web app

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ArticleCard
+      title="Introducing Vue 3 Composition API"
+      summary="Learn about Vue 3's new Composition API and how it improves code organization."
+      link="https://vuejs.org/guide/explanation/composition-api.html"
+    ></ArticleCard>
+  </div>
+</template>
+
+<script setup>
+import ArticleCard from './components/ArticleCard.vue'
+</script>
